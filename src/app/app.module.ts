9c7aa91d3677675fe0a59cfd12da377174c7e375
HienVDRK <hienvd@rikkeisoft.com>
2020-03-23 18:18:18 +0700

    add prj font end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './components/account/account.component';
+import { RouterModule } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: AccountComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'login', component: AuthComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
